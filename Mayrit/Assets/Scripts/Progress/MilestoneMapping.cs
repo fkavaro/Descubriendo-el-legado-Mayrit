b5ee92f8c8faf099ca71f732fb3a3a0a2a45fcbd
fkavaro <by.fka.varo@gmail.com>
2025-11-25 19:26:33 +0100

    *

--- Mayrit/Assets/Scripts/Progress/MilestoneMapping.cs
+++ Mayrit/Assets/Scripts/Progress/MilestoneMapping.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class MilestoneMapping
 {
     [SerializeField] Milestone_InformationSO _milestoneData;
-    [SerializeField] GameObject _playableCharacterGO;
+    [SerializeField] GameObject _playableCharacter;
     [SerializeField] GameObject _tour;
 
     public Milestone_InformationSO Data => _milestoneData;
-    public PlayableCharacter PlayableCharacter => _playableCharacterGO.GetComponent<PlayableCharacter>();
+    public PlayableCharacter PlayableCharacter => _playableCharacter.GetComponent<PlayableCharacter>();
     public Tour Tour => _tour.GetComponent<Tour>();
 }
